8293c95b7f31353834f3c583d820ee4b6960c048
Dhaiwat10 <dhaiwatpandya@gmail.com>
2021-01-26 23:55:26 +0530

    Add support for what's happening

--- content.js
+++ content.js
@@ -1,22 +1,15 @@
-let lastUrl = location.href;
 new MutationObserver(() => {
-  const url = location.href;
-  if (url !== lastUrl) {
-    lastUrl = url;
-    onUrlChange();
-  }
+  onDOMChange();
 }).observe(document, { subtree: true, childList: true });
 
-function onUrlChange() {
-  beginCleanup();
+function onDOMChange() {
+  beginCleanup(true);
 }
 
 const beginCleanup = () => {
   console.log('Beginning cleanup');
-
-  setTimeout(() => {
-    removeWhoToFollow();
-  }, 2000);
+  removeWhoToFollow();
+  removeHappening();
 };
 
 const removeWhoToFollow = () => {
@@ -28,16 +21,36 @@ const removeWhoToFollow = () => {
   const aside = div2.parent('aside');
   const div3 = aside.parent('div');
 
-  console.log(span);
-  console.log(div1);
-  console.log(h2);
-  console.log(div2);
-  console.log(aside);
-  console.log(div3);
+  // console.log(span);
+  // console.log(div1);
+  // console.log(h2);
+  // console.log(div2);
+  // console.log(aside);
+  // console.log(div3);
 
   div3.remove();
 };
 
-const removeHappening = () => {};
+const removeHappening = () => {
+  // span > div > h2 > div > div > div > div
+
+  const span = $("span:contains('What’s happening')");
+  const div1 = span.parent('div');
+  const h2 = div1.parent('h2');
+  const div2 = h2.parent('div');
+  const div3 = div2.parent('div');
+  const div4 = div3.parent('div');
+  const div5 = div4.parent('div');
+
+  // console.log(span);
+  // console.log(div1);
+  // console.log(h2);
+  // console.log(div2);
+  // console.log(div3);
+  // console.log(div4);
+  // console.log(div5);
+
+  div5.remove();
+};
 
 document.onload = beginCleanup();
