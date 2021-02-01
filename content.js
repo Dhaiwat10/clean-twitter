e6f678b44c74c819cbaaef94e6cb9ea981f2fb8d
Dhaiwat10 <dhaiwatpandya@gmail.com>
2021-02-02 03:02:16 +0530

    Add boilerplate for numbers clean-up and footer clean-up

--- content.js
+++ content.js
@@ -6,13 +6,21 @@ function onDOMChange() {
   beginCleanup(true);
 }
 
+const checkIfNumber = val => {
+  let isNum = /^\d+$/.test(val);
+  return isNum;
+};
+
 const beginCleanup = () => {
-  console.log('Beginning cleanup');
+  // console.log('Beginning cleanup');
   removeSection(['div', 'h2', 'div', 'aside', 'div'], 'Who to follow');
   removeSection(['div', 'h2', 'div', 'div', 'div', 'div'], 'What’s happening');
   removeSection(['div', 'h2', 'div', 'aside', 'div'], 'You might like');
   removeSection(['div', 'h2', 'div', 'aside', 'div'], 'Relevant people');
-  // removeSection([], 'Who to follow');
+
+  removeNumbers();
+
+  removeFooter();
 };
 
 const removeSection = (treeStructure, targetText) => {
@@ -29,6 +37,31 @@ const removeSection = (treeStructure, targetText) => {
   // console.log('Itr after loop completion', iterator);
 
   iterator.remove();
+};
+
+const removeNumbers = () => {
+  const likesEl = $(
+    '.css-901oao, .css-16my406, .r-poiln3, .r-bcqeeo, .r-qvutc0'
+  );
+
+  for (let item of likesEl) {
+    const { nodeName: tag } = item;
+
+    if (tag === 'SPAN') {
+      const innerText = $(item).html();
+
+      if (checkIfNumber(innerText)) {
+        // TODO: Also handle numbers with suffixes like K, M (eg. 100k, 1M)
+        item.remove();
+      }
+    }
+  }
+};
+
+const removeFooter = () => {
+  const navElement = $('[aria-label=Footer]');
+
+  navElement.remove();
 };
 
 document.onload = beginCleanup();
