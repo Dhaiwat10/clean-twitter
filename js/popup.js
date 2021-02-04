d75b9a01e68f0082209e3b774f6e64098a2cc10b
Dhaiwat10 <dhaiwatpandya@gmail.com>
2021-02-04 20:58:40 +0530

    Add auto-reload on enable/disable

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,22 @@
 var enabled = false; //disabled by default
 var myButton = document.getElementById('toggle');
 
+let tab = null;
+
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabArray) {
+  tab = tabArray[0];
+});
+
 chrome.storage.local.get('enabled', data => {
   enabled = !!data.enabled;
   myButton.textContent = enabled ? 'Disable' : 'Enable';
 });
 
 myButton.onclick = () => {
+  // Reload on enable/disable
+  const code = 'window.location.reload();';
+  chrome.tabs.executeScript(tab.id, { code: code });
+
   enabled = !enabled;
   myButton.textContent = enabled ? 'Disable' : 'Enable';
   chrome.storage.local.set({ enabled: enabled });
