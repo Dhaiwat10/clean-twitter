035f1010d33fe9bc31da017698fed3c73eabfb58
Dhaiwat10 <dhaiwatpandya@gmail.com>
2021-02-04 21:10:47 +0530

    Add styled toggle button

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,6 @@
 var enabled = false; //disabled by default
-var myButton = document.getElementById('toggle');
+var checkbox = document.getElementById('checkbox');
+var label = document.getElementById('label');
 
 let tab = null;
 
@@ -9,15 +10,16 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabArray) {
 
 chrome.storage.local.get('enabled', data => {
   enabled = !!data.enabled;
-  myButton.textContent = enabled ? 'Disable' : 'Enable';
+  label.textContent = enabled ? 'Enabled' : 'Disabled';
+  checkbox.checked = enabled;
 });
 
-myButton.onclick = () => {
+checkbox.onclick = () => {
   // Reload on enable/disable
   const code = 'window.location.reload();';
   chrome.tabs.executeScript(tab.id, { code: code });
 
   enabled = !enabled;
-  myButton.textContent = enabled ? 'Disable' : 'Enable';
+  label.textContent = enabled ? 'Enabled' : 'Disabled';
   chrome.storage.local.set({ enabled: enabled });
 };
