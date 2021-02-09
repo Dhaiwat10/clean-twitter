faf5a32040c748358e8ba8cf9bb6ba64018321bb
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-02-09 19:10:30 +0530

    Improve refresh UX #4

--- js/popup.js
+++ js/popup.js
@@ -8,7 +8,7 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabArray) {
   tab = tabArray[0];
 });
 
-chrome.storage.local.get('enabled', data => {
+chrome.storage.local.get('enabled', (data) => {
   enabled = !!data.enabled;
   label.textContent = enabled ? 'Enabled' : 'Disabled';
   checkbox.checked = enabled;
@@ -17,9 +17,32 @@ chrome.storage.local.get('enabled', data => {
 checkbox.onclick = () => {
   // Reload on enable/disable
   const code = 'window.location.reload();';
-  chrome.tabs.executeScript(tab.id, { code: code });
+
+  const hostname = getDomain(tab.url);
+
+  if (hostname === 'twitter.com') {
+    chrome.tabs.executeScript(tab.id, { code: code });
+  }
 
   enabled = !enabled;
   label.textContent = enabled ? 'Enabled' : 'Disabled';
   chrome.storage.local.set({ enabled: enabled });
 };
+
+function getDomain(url, subdomain) {
+  subdomain = subdomain || false;
+
+  url = url.replace(/(https?:\/\/)?(www.)?/i, '');
+
+  if (!subdomain) {
+    url = url.split('.');
+
+    url = url.slice(url.length - 2).join('.');
+  }
+
+  if (url.indexOf('/') !== -1) {
+    return url.split('/')[0];
+  }
+
+  return url;
+}
